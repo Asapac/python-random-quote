4dc53f1297f14279fb6f992f1d8aafebbb017432
Asapac <pacmacfeast@hotmail.com>
2021-05-09 17:21:31 +0100

    Add some more quotes, Print out more than one quote at a time, Remove new line

--- get-quote.py
+++ get-quote.py
@@ -1,15 +1,21 @@
 import random
 
 def primary():
-  #print("Keep it logically awesome.")
+  # print("Keep it logically awesome.")
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
-  last = len(quotes) - 1
-  rnd = random.randint(0, last)
-  print(quotes[rnd])
+  # Original usage
+  # last = len(quotes) - 1
+  # rnd = random.randint(0, last)
+  # print(quotes[rnd], end="")
+
+  lines = 2
+  quotes_sq = random.choices(quotes, k=lines)
+  # print(*quotes_sq, sep='\n')
+  print(*quotes_sq, end="")
 
 if __name__== "__main__":
   primary()
